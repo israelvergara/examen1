3dfee59adba2f23d9a4d4d056e60f47c2df165c5
Israel Vergara <israel.vergara102824@gmail.com>
2017-09-04 09:59:43 -0600

    mdi

--- Estacionamiento.java
+++ Estacionamiento.java
@@ -5,7 +5,21 @@ public class Estacionamiento
     
     public Estacionamiento()
     {
-        vehiculos[150]; 
+        Vehiculo[] vehiculos= new Vehiculo[150];
         
+    }
+    
+    public void espacio()
+    {
+        if(vehiculos.length > 150)
+        {
+            System.out.println("Estacionamiento Lleno");
+        }else
+        {
+            for(int i=0; i < vehiculos.length; i++)
+            {
+                //vehiculos[i].registro(vehicu);
+            }
+        }
     }
 }
